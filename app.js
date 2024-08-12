b0b414923d0ed70804cddf7200221c084c0b492d
dharanidharanu <dharanidharan.u2022eee@sece.ac.in>
2024-08-12 09:18:37 +0530

    feat: cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express =require("express")
 const app = express()
 app.use(express.json())
+app.use(cors())
 const productRoutes = require("./routes/productRoutes")
 const userRoutes = require("./routes/userRoutes")
 const cartRoutes=require("./routes/cartRoutes")
