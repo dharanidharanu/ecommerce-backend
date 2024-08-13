03a3d151bb21396b3c8fe2791a009ee9438b7936
dharanidharanu <dharanidharan.u2022eee@sece.ac.in>
2024-08-13 09:25:41 +0530

    Fix:products

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,7 +2,7 @@ const productController = require("../controllers/productController")
 const express = require("express")
 const router = express.Router()
 const auth = require("../middleware/auth")
-router.get("/",auth,productController.getProducts)
+router.get("/getProducts",productController.getProducts)
 router.post("/",auth,productController.createProducts)
 
 module.exports=router
